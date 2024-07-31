b2f253924e8eb2e4948ad49db8a46243393a0268
chamdom <wjdtmdry88@gmail.com>
2024-07-31 14:32:05 +0900

    fix : yml

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+  experimental: {
+    domains: [
+      "www.notion.so",
+      "lh5.googleusercontent.com",
+      "s3-us-west-2.amazonaws.com",
+    ],
   },
 }
